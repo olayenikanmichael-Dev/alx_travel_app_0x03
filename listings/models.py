1cc87a6ec5a65cdf85467e02f046a633a7fd511f
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2026-01-12 19:01:50 +0100

    backend

--- listings/models.py
+++ listings/models.py
@@ -2,23 +2,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Payment(models.Model):
-    STATUS_CHOICES = (
-        ('Pending', 'Pending'),
-        ('Completed', 'Completed'),
-        ('Failed', 'Failed'),
-    )
-
-    booking_reference = models.CharField(max_length=100, unique=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_id = models.CharField(max_length=255, blank=True, null=True)
-    status = models.CharField(
-        max_length=20,
-        choices=STATUS_CHOICES,
-        default='Pending'
-    )
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.booking_reference} - {self.status}"
+        return self.title
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="bookings"
+    )
+    listing = models.ForeignKey(
+        Listing,
+        on_delete=models.CASCADE,
+        related_name="bookings"
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Booking by {self.user.username} for {self.listing.title}"
--- listings/views.py
+++ listings/views.py
@@ -1,19 +1,8 @@
-from django.shortcuts import render
-from .tasks import send_booking_confirmation_email
-
-
 from rest_framework import viewsets
+
 from .models import Listing, Booking
 from .serializers import ListingSerializer, BookingSerializer
-import uuid
-import requests
-from django.conf import settings
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
 
-from .models import Payment
-# Create your views here.
 
 class ListingViewSet(viewsets.ModelViewSet):
     """
